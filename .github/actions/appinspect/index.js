53dee71406fc657f8238aa57bc549299465c58cd
Daniel Federschmidt <dfederschmidt@splunk.com>
2021-02-16 17:14:15 +0100

    clean up app

--- .github/actions/appinspect/index.js
+++ .github/actions/appinspect/index.js
@@ -2,13 +2,11 @@ const core = require('@actions/core');
 const axios = require('axios')
 var FormData = require('form-data');
 var fs = require('fs');
-const { report } = require('process');
 const path = require('path');
 
-const authURL = "https://api.splunk.com/2.0/rest/login/splunk"
-const submitURL = "https://appinspect.splunk.com/v1/app/validate"
-
 async function getToken() {
+    const authURL = "https://api.splunk.com/2.0/rest/login/splunk"
+
     try {
         const response = await axios({
             url: authURL,
@@ -26,6 +24,7 @@ async function getToken() {
 }
 
 async function submitApp(token, bundlePath) {
+    const submitURL = "https://appinspect.splunk.com/v1/app/validate"
     var formData = new FormData();
     formData.append('app_package', fs.createReadStream(bundlePath));
 
@@ -48,7 +47,6 @@ async function submitApp(token, bundlePath) {
 
 async function checkStatus(token, requestID) {
     const statusURL = `https://appinspect.splunk.com/v1/app/validate/status/${requestID}`
-
     try {
         const response = await axios({
             url: statusURL,
@@ -57,16 +55,14 @@ async function checkStatus(token, requestID) {
                 Authorization: `bearer ${token}`,
             }
         })
-
         return response.data
-
     } catch (error) {
         console.log(error.response)
     }
-    
+
 }
 
-async function checkReport(token, requestID, reportPath) {
+async function fetchReport(token, requestID, reportPath) {
     const reportURL = `https://appinspect.splunk.com/v1/app/report/${requestID}`
 
     try {
@@ -75,7 +71,7 @@ async function checkReport(token, requestID, reportPath) {
             method: 'get',
             headers: {
                 Authorization: `bearer ${token}`,
-                'Content-Type': 'text/html', 
+                'Content-Type': 'text/html',
             }
         })
         fs.writeFile(path.join(reportPath, 'report.html'), response.data, (err) => {
@@ -89,42 +85,22 @@ async function checkReport(token, requestID, reportPath) {
 
 }
 
-async function pollStatus(token, requestID) {
-    const interval = 15000
-
-    async function checkFinished() {
-        const result = await checkStatus(token, requestID)
-        console.log(result.status)
-        if (result.status !== "SUCCESS") {
-            setTimeout(checkFinished, interval)
-        } else {
-            return result
-        }
-    }
-    return await checkFinished()
-}
-
-
-function poll(fn, token, requestID,timeout, interval) {
+function pollStatus(fn, token, requestID, timeout, interval) {
     var endTime = Number(new Date()) + (timeout || 2000);
     interval = interval || 100;
 
-    var checkCondition = function(resolve, reject) { 
-        var ajax = fn(token, requestID);
-        // dive into the ajax promise
-        ajax.then( function(response){
-            // If the condition is met, we're done!
+    var checkCondition = function (resolve, reject) {
+        var fetchStatus = fn(token, requestID);
+        fetchStatus.then(function (response) {
             console.log(response.status)
 
-            if(response.status == "SUCCESS") {
+            if (response.status == "SUCCESS") {
                 console.log(response.info)
                 resolve(response.data);
             }
-            // If the condition isn't met but the timeout hasn't elapsed, go again
             else if (Number(new Date()) < endTime) {
                 setTimeout(checkCondition, interval, resolve, reject);
             }
-            // Didn't match and too much time, reject!
             else {
                 reject(new Error('timed out for ' + fn + ': ' + arguments));
             }
@@ -134,18 +110,15 @@ function poll(fn, token, requestID,timeout, interval) {
     return new Promise(checkCondition);
 }
 
-
-
 async function run() {
     const bundlePath = core.getInput('app-bundle');
     const reportPath = core.getInput('report-path');
 
     token = await getToken()
     requestID = await submitApp(token, bundlePath)
-    
-    poll(checkStatus, token, requestID, 1000000, 10000).then(() =>{
-        console.log("Done polling")
-        checkReport(token, requestID, reportPath)
+
+    pollStatus(checkStatus, token, requestID, 1000000, 10000).then(() => {
+        fetchReport(token, requestID, reportPath)
     }).catch(err => {
         console.log(err)
     })
